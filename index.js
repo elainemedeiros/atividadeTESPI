6274ce1a621b28910e7492fbe68f7615749aedd8
Elaine Medeiros <108952213+elainemedeiros@users.noreply.github.com>
2023-04-22 22:00:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hi Docker!!!");
+  res.send("Hello Sistemas para Internet - P3 - Noite. A melhor turma.");
 });
 
 app.listen(3000);
